17c00ae9d881c01315cab1838106d506371a336c
Luiz Henrique <luiz.contato.1000@gmail.com>
2026-02-04 16:03:14 -0300

    feat: setup routes componets and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function App() {
   return <RouterProvider router={router}></RouterProvider>
--- src/pages/RootLayout.jsx
+++ src/pages/RootLayout.jsx
@@ -1,8 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 export default function RootLayout() {
   return (
     <div className="container-fluid">
-        <header>
-            <nav className="nav d-flex  justify-content-between">
+        <header className="header">
+            <nav className="nav d-flex justify-content-between">
                 <h1>React Stock</h1>
                 <button>Inicío</button> // esse butom vai ter um link que vai levar ao Dashboard com Link do react router
                 <button>Items</button> // aqui tb
